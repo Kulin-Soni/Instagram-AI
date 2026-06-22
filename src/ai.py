2c70904d5073fbf875788854a71cdee8d0141a4b
Kulin-Soni <grozersniper38237@gmail.com>
2026-06-22 15:26:31 +0530

    Fixed python version, waits for ai models to be ready before logging in.

--- src/ai.py
+++ src/ai.py
@@ -1,6 +1,12 @@
-from ollama import AsyncClient
+import asyncio
 
+import httpx
+import logging
+from ollama import AsyncClient
 from config import OLLAMA_HOST, OLLAMA_MODEL
+
+logger = logging.getLogger(__name__)
+
 class AISession:
     def __init__(self) -> None:
         self.client = AsyncClient(
@@ -18,3 +24,18 @@ class AISession:
             return res.message.content
         except Exception:
             return
+
+    @staticmethod
+    async def wait_for_ready():
+        async with httpx.AsyncClient() as session:
+            while True:
+                try:
+                    res = await session.get(f'{OLLAMA_HOST}/api/tags')
+                    res.raise_for_status()
+                    tags = res.json()
+                    if tags:
+                        logger.info("Ollama is ready!")
+                        return
+                except httpx.HTTPStatusError:
+                    logger.info("Waiting for ollama to be ready...")
+                    await asyncio.sleep(1)
--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ import logging
 
 from beanie import init_beanie
 from pymongo import AsyncMongoClient
+from ai import AISession
 from config import MONGO_URI
 from instagram import InstagramClient
 from models import InstagramChat
@@ -22,6 +23,7 @@ async def main():
         format="[%(asctime)s - %(levelname)s] %(name)s: %(message)s",
     )
     await init_mongo()
+    await AISession.wait_for_ready()
     async with InstagramClient() as client:
         await client.listen_for_messages()
 
